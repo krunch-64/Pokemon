769fbb4052b89a08fbc6090ef3e2c5abfca46fa0
Baptiste Lecordier <lecordierbaptiste@gmail.com>
2022-11-22 15:01:01 +0100

    truc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
+
+
+
 var btnAttack = document.getElementById('btn-attack');
+var btnAttack1 = document.getElementById('btn-attack1');
 var btnPkmn = document.getElementById('btn-pkmn');
+var btnPkmn1 = document.getElementById('btn-pkmn1');
+var btnPkmn2 = document.getElementById('btn-pkmn2');
 var btnQuit = document.getElementById('btn-quit');
 var btnBack1 = document.getElementById('btn-back1');
 var btnBack2 = document.getElementById('btn-back2');
@@ -10,12 +16,20 @@ var menuArena2 = document.getElementById('menu-arena-2');
 var menuArena3 = document.getElementById('menu-arena-3');
 var menuArena4 = document.getElementById('menu-arena-4');
 
+var pkmn1 = document.getElementById('menu-arena-1');
+var pkmn2 = document.getElementById('menu-arena-2');
+
+
+
+
+
 var popUP = document.getElementById('popup');
 
 
 btnAttack.addEventListener("click", function () {
     menuArena1.classList.add('d-none');
     menuArena2.classList.remove('d-none');
+
 }); 
 
 btnBack1.addEventListener("click", function () {
@@ -32,6 +46,7 @@ btnPkmn.addEventListener("click", function () {
 btnBack2.addEventListener("click", function () {
     menuArena3.classList.add('d-none');
     menuArena1.classList.remove('d-none');
+    
 }); 
 
 btnQuit.addEventListener("click", function () {
@@ -42,4 +57,49 @@ btnQuit.addEventListener("click", function () {
 btnBack3.addEventListener("click", function () {
     menuArena4.classList.add('d-none');
     menuArena1.classList.remove('d-none');
+}); 
+
+btnAttack1.addEventListener("click", function () {
+    menuArena1.classList.add('d-none');
+    menuArena2.classList.add('d-none');
+    menuArena3.classList.add('d-none');
+    menuArena4.classList.add('d-none');
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.querySelector('#test').innerHTML = this.responseText;
+    };
+    xhttp.open("GET", "send_request_ajax.php?action=attack1", true);
+    xhttp.send();
+}); 
+
+
+btnPkmn1.addEventListener("click", function () {
+    menuArena1.classList.add('d-none');
+    menuArena2.classList.add('d-none');
+    menuArena3.classList.add('d-none');
+    menuArena4.classList.add('d-none');
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.querySelector('#test').innerHTML = this.responseText;
+    };
+    xhttp.open("GET", "send_request_ajax.php?action=pkmn1", true);
+    xhttp.send();
+}); 
+
+btnPkmn2.addEventListener("click", function () {
+    menuArena1.classList.add('d-none');
+    menuArena2.classList.add('d-none');
+    menuArena3.classList.add('d-none');
+    menuArena4.classList.add('d-none');
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.querySelector('#test').innerHTML = this.responseText;
+    };
+    xhttp.open("GET", "send_request_ajax.php?action=pkmn2", true);
+    xhttp.send();
 }); 
---
+++ send_request_ajax.php
@@ -0,0 +1,25 @@
+<?php 
+    if (isset($_GET['action']))
+
+    {
+        if ($_GET['action'] == 'attack1')
+
+        {
+            echo 'test';
+        }
+
+        if ($_GET['action'] == 'pkmn1')
+
+        {
+            echo 'g';
+        }
+
+        if ($_GET['action'] == 'pkmn2')
+
+        {
+            echo 'ee';
+        }
+    }
+?>
+
+
