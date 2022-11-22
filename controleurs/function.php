0fdb942eb03e243ce346bf0dc40a6265d0aa9d47
Romain Bailhache <romain.bailhache@gmail.com>
2022-11-22 14:08:48 +0100

    Update function.php

--- controleurs/function.php
+++ controleurs/function.php
@@ -10,7 +10,7 @@ function request(string $url)
 
 
 function get_pokemon_list() {
-    $result = request('https://pokeapi.co/api/v2/pokemon?offset=30&limit=30');
+    $result = request('https://pokeapi.co/api/v2/pokemon?offset=0&limit=24');
 
     $pokemons =  $result->{'results'};
     foreach($pokemons as $pokemon) {
@@ -19,12 +19,12 @@ function get_pokemon_list() {
         $name = $pokemon->{'name'};
         ?> <div class="card ">
             <img alt='<?= $name ?>' src="<?= get_Sprites($id,'front') ?>">
-            <p><?= translate_name_pokemon($name) ?></p>
+            <p alt='<?= $id ?>'><?= translate_name_pokemon($name) ?></p>
         </div> <?php 
     }
 };
 
-
+get_pokemon_list();
 /** La fonction get_front_sprites permet de récupré la front sprites d'un pokemon
  * @param string $id d'un pokemon
  * @param string $frame 'front' ou 'back' 
@@ -47,22 +47,20 @@ function get_pokemon_stat(string $id)
     $parsed_json = request('https://pokeapi.co/api/v2/pokemon/'.$id);
 
 
-    $table = array(
-    'id' =>  $id,
+    var_dump(request($parsed_json->{'types'}[0]->{'type'}->{'url'})->{'damage_relations'}) ;
+    $table = [
+    'id' =>  (int)$id,
     'name' => $parsed_json->{'name'},
     'element_primary' => $parsed_json->{'types'}[0]->{'type'}->{'name'},
     'element_secondary' => $parsed_json->{'types'}[1]->{'type'}->{'name'},
     'pv' => $parsed_json->{'stats'}[0]->{'base_stat'},
     'attack' => $parsed_json->{'stats'}[1]->{'base_stat'},
     'defense' => $parsed_json->{'stats'}[2]->{'base_stat'},
-    'special_attack' => $parsed_json->{'stats'}[3]->{'base_stat'},
-    'special_defense' => $parsed_json->{'stats'}[4]->{'base_stat'},
-    'speed' => $parsed_json->{'stats'}[5]->{'base_stat'}
-    );
+    ];
     return $table;
 }
 
-var_dump(get_pokemon_list(50));
+get_pokemon_stat(48);
 /** la fonction translate_name_pokemon permet de traduire le nom de pokemon en anglais en français
  * @param string $response page pokemon
  * @return string $name pokemon in french
