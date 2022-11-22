d14c9b999fd6ab84e01d9459b70b6a5434226e60
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2022-11-22 13:47:55 +0100

    controleur insert

--- controleurs/CtrlPokemon.php
+++ controleurs/CtrlPokemon.php
@@ -1,30 +1,7 @@
 <?php
     // Rôle : afficher la liste des pokémon
 
-    // Récupération des données de l'api
-    include_once('./function.php');
-
-    // connexion du serveur web à la base MySQL
-    include_once ('../modeles/DAO.php');
-
-    $i = 1;
-    while ($i <=24)
-    {
-        $tablePokemon = get_pokemon_stat($i);
-        $dao = new DAO();
-        
-        $ex = $dao->insertPokemon($tablePokemon);
-        echo $ex;
-
-        $i++;
-    }
-
-    // var_dump($tablePokemon);
-
     $dao = new DAO();
-
-    // $ex = $dao->insertPokemon($tablePokemon);
-    // echo $ex;
 
     // récupération du tableau des courses
     $tabCourses = $dao->getListePokemon();
--- modeles/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include_once('./DAO.php');
-    $dao = new DAO();
-    $ec = $dao->insertPokemon()
-?>  
