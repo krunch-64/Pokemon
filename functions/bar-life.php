c274ab3ef6f9e6d57c0400280f75b43e15c45625
Baptiste Lecordier <lecordierbaptiste@gmail.com>
2022-11-23 14:00:31 +0100

    f

--- functions/bar-life.php
+++ functions/bar-life.php
@@ -5,7 +5,14 @@
     function get_life_for_bar ($max_life,$life)
 
     {
-        return (round((20 * $life) / $max_life))*5;
+        $return = (round((20 * $life) / $max_life))*5;
+
+
+        if ($return > 0) {
+            return $return;
+        } else {
+            return 0;
+        }
     }
 
     echo get_life_for_bar ($max_life,$life);
